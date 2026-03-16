e23ec103afed0d80409615c6866c9d82581f06be
Malsgr <tomaspmdc@gmail.com>
2026-03-16 23:01:00 +0000

    el script por fin funciona

--- install-linux.sh
+++ install-linux.sh
@@ -8,6 +8,24 @@ cp snippets/sql.json "$CONFIG_DIR/snippets/sql.json"
 cp snippets/html.json "$CONFIG_DIR/snippets/html.json"
 cp snippets/javascript.json "$CONFIG_DIR/snippets/javascript.json"
 cp snippets/conectarse.json "$CONFIG_DIR/snippets/conectarse.json"
-cp snippets/pagina.json "$CONFIG_DIR/snippets/pagina.json"
+
+python3 -c "
+import json
+
+# Read html.json
+with open('snippets/html.json', 'r', encoding='utf-8') as f:
+    html_snippets = json.load(f)
+
+# Read pagina.json
+with open('snippets/pagina.json', 'r', encoding='utf-8') as f:
+    pagina_snippets = json.load(f)
+
+# Merge pagina.json into html.json
+html_snippets.update(pagina_snippets)
+
+# Write to destination
+with open('$CONFIG_DIR/snippets/html.json', 'w', encoding='utf-8') as f:
+    json.dump(html_snippets, f, indent=4, ensure_ascii=False)
+"
 
 echo "Snippets installed to $CONFIG_DIR/snippets/"
